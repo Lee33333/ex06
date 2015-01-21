7e8da53afe99aa966eefd6daed2c10ad29c418a8
Hackbright Student <info@hackbrightacademy.com>
2015-01-20 17:55:15 -0800

    in function format

--- sorting.py
+++ sorting.py
@@ -1,21 +1,43 @@
+def file_to_dict(files):
+    file_dict = {}
+    score_file = open(files)
+    for line in score_file:
+        line = line.rstrip()
+        line = line.split(":")
+        file_dict[line[0]] = line[1] 
+    return file_dict
 
-# # setup
-# create an empty scores dictionary
-restaurant_scores = {}
-# open the file 'sorted.txt'
-score_file = open("scores.txt")
-# # main loop
-# for each line in the file:
-for line in score_file:
-#     split the line by the separator ":"
-    line = line.rstrip()
-    line = line.split(":")
-#     using the first token as a key, insert the second token into the dictionary as a value
-    restaurant_scores[line[0]] = line[1] 
+def sorting(dictss):
+    listkeys = sorted(dictss)
+    for item in listkeys:
+        print "Restaurant: %s is rated at %s " % (item, dictss[item])
+
+def main():
+    file_dict = file_to_dict("scores.txt")
+    sorting_file = sorting(file_dict)
+    return sorting_file
+
+if __name__ == "__main__":
+    main()
+
+# # # setup
+# # create an empty scores dictionary
+# restaurant_scores = {}
+# # open the file 'sorted.txt'
+# score_file = open("scores.txt")
+# # # main loop
+# # for each line in the file:
+# for line in score_file:
+# #     split the line by the separator ":"
+#     line = line.rstrip()
+#     line = line.split(":")
+# #     using the first token as a key, insert the second token into the dictionary as a value
+#     restaurant_scores[line[0]] = line[1] 
 
 # create a list of keys
-listkeys = sorted(restaurant_scores)
-# for each key in the sorted keys:
-for item in listkeys:
-#     print the restaurant name and score
-    print "Restaurant: %s is rated at %s " % (item, restaurant_scores[item])
+# listkeys = sorted(file_dict)
+# print listkeys
+# # for each key in the sorted keys:
+# for item in listkeys:
+# #     print the restaurant name and score
+#     print "Restaurant: %s is rated at %s " % (item, file_dict[item])
